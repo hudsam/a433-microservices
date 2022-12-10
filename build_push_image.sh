6b36e0e90c8a07aab9df2c8046a6f51fae70cde1
hudsam <app.hoedhya@gmail.com>
2022-12-10 10:58:49 +0700

    Added simple comment(s)

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# Inisialisasi variabel untuk nama image dan versi
 $__namedImage="item-app:v1"
 
+# Perintah untuk membuat image dari Dockerfile
 docker build -t $__namedImage --file Dockerfile .
 
+# Menampilkan daftar image yang ada dengan kriteria tertentu
 docker images --all | awk '{ print $1 " / "  $2 }' | grep "item-*
 
+# Duplikasi image yang sebelumnya dibuat sesuai format penamaan registry
 docker tag $__namedImage $__usernameDockerHub/$__namedImage
 
+# Melakukan autentikasi akun ke registry
 echo "$__passwordDockerHub" | docker login --username $__usernameDockerHub --password-stdin
 
+# Mengunggah image ke namespace
 docker push $__usernameDockerHub/$__namedImage
 
